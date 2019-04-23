a13d0919b4e3a945d5f77aa1b2796c0279a49886
felixire <felixire@protonamil.com>
2019-04-23 21:45:33 +0200

    Added new Project Added Chest Percantage Modifier

---
+++ Chest Percantage Modifier/Config.cs
@@ -0,0 +1,112 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using UnityEngine;
+
+namespace Chest_Percantage_Modifier
+{
+    class Config
+    {
+        #region Wrappers
+        private static ConfigWrapper<int> Tier1Percantage_Normal_Conf { get; set; }
+        private static ConfigWrapper<int> Tier2Percantage_Normal_Conf { get; set; }
+        private static ConfigWrapper<int> Tier3Percantage_Normal_Conf { get; set; }
+
+        private static ConfigWrapper<int> Tier1Percantage_Large_Conf { get; set; }
+        private static ConfigWrapper<int> Tier2Percantage_Large_Conf { get; set; }
+        private static ConfigWrapper<int> Tier3Percantage_Large_Conf { get; set; }
+
+        private static ConfigWrapper<int> Tier1Percantage_Golden_Conf { get; set; }
+        private static ConfigWrapper<int> Tier2Percantage_Golden_Conf { get; set; }
+        private static ConfigWrapper<int> Tier3Percantage_Golden_Conf { get; set; }
+        #endregion
+
+        List<ConfigWrapper<int>> configs = new List<ConfigWrapper<int>>();
+        List<float> normalizedConfigs = new List<float>();
+        public void Init(ConfigFile Config)
+        {
+            Tier1Percantage_Normal_Conf = Config.Wrap<int>("Normal Chest", "Tier 1 Percantage", "How likely it is that tier 1 pops up on a Normal chest (0-100) Default: 80", 80);
+            Tier2Percantage_Normal_Conf = Config.Wrap<int>("Normal Chest", "Tier 2 Percantage", "How likely it is that tier 2 pops up on a Normal chest (0-100) Default: 20", 20);
+            Tier3Percantage_Normal_Conf = Config.Wrap<int>("Normal Chest", "Tier 3 Percantage", "How likely it is that tier 3 pops up on a Normal chest (0-100) Default: 01", 01);
+
+            Tier1Percantage_Large_Conf = Config.Wrap<int>("Large Chest", "Tier 1 Percantage", "How likely it is that tier 1 pops up on a Large chest (0-100) Default: 00", 0);
+            Tier2Percantage_Large_Conf = Config.Wrap<int>("Large Chest", "Tier 2 Percantage", "How likely it is that tier 2 pops up on a Large chest (0-100) Default: 80", 80);
+            Tier3Percantage_Large_Conf = Config.Wrap<int>("Large Chest", "Tier 3 Percantage", "How likely it is that tier 3 pops up on a Large chest (0-100) Default: 20", 20);
+
+            Tier1Percantage_Golden_Conf = Config.Wrap<int>("Golden Chest", "Tier 1 Percantage", "How likely it is that tier 1 pops up on a Golden chest (0-100) Default: 000", 000);
+            Tier2Percantage_Golden_Conf = Config.Wrap<int>("Golden Chest", "Tier 2 Percantage", "How likely it is that tier 2 pops up on a Golden chest (0-100) Default: 000", 000);
+            Tier3Percantage_Golden_Conf = Config.Wrap<int>("Golden Chest", "Tier 3 Percantage", "How likely it is that tier 3 pops up on a Golden chest (0-100) Default: 100", 100);
+
+            configs.Add(Tier1Percantage_Normal_Conf);
+            configs.Add(Tier2Percantage_Normal_Conf);
+            configs.Add(Tier3Percantage_Normal_Conf);
+
+            configs.Add(Tier1Percantage_Large_Conf);
+            configs.Add(Tier2Percantage_Large_Conf);
+            configs.Add(Tier3Percantage_Large_Conf);
+
+            configs.Add(Tier1Percantage_Golden_Conf);
+            configs.Add(Tier2Percantage_Golden_Conf);
+            configs.Add(Tier3Percantage_Golden_Conf);
+
+            NormalizeValues();
+        }
+
+        private void NormalizeValues()
+        {
+            normalizedConfigs.Clear();
+            for (int i = 0; i < configs.Count; i++)
+            {
+                normalizedConfigs.Add(configs[i].Value);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                float sum = configs[i * 3 + 0].Value + configs[i * 3 + 1].Value + configs[i * 3 + 2].Value;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (sum <= 100)
+                    {
+                        float unNormalized = configs[i * 3 + j].Value;
+                        normalizedConfigs[i * 3 + j] = unNormalized;
+                    }
+                    else
+                    {
+                        float unNormalized = configs[i * 3 + j].Value;
+                        float normalized = (100 / sum * unNormalized);
+                        normalizedConfigs[i * 3 + j] = normalized;
+                    }
+                }
+
+            }
+        }
+
+        public float GetValue(int tier, ChestType chestType)
+        {
+            if (tier < 1 || tier > 3)
+                return -1;
+
+            int index = (int)chestType * 3 + tier - 1;
+
+            if (index > 8)
+            {
+                Debug.Log("Invalid Index when getting Chest percantage...");
+                return -1;
+            }
+
+            float value = normalizedConfigs[index];
+            //Debug.Log("Unnormalized: " + configs[index].Value);
+            //Debug.Log("Normalized: " + normalizedConfigs[index]);
+            return value;
+        }
+
+        public enum ChestType
+        {
+            Normal,
+            Large,
+            Golden
+        }
+    }
+}
---
+++ Chest Percantage Modifier/Main.cs
@@ -0,0 +1,168 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+using RoR2.UI;
+using BepInEx.Configuration;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using EntityStates;
+using System.Collections.Generic;
+
+namespace Chest_Percantage_Modifier
+{
+    [BepInPlugin("dev.felixire.ChestPercantage", "ChestPercantage", "1.0.0")]
+    class Main : BaseUnityPlugin
+    {
+        public Config config;
+        public System.Random random;
+        public bool hasSet = false;
+
+        public void Awake()
+        {
+            config = new Config();
+            config.Init(Config);
+
+            var epochStart = new System.DateTime(1970, 1, 1, 8, 0, 0, System.DateTimeKind.Utc);
+            int seed = (int)((System.DateTime.UtcNow - epochStart).TotalSeconds / 2);
+            Debug.Log(seed);
+            random = new System.Random(seed);
+
+            On.RoR2.ChestBehavior.ItemDrop += ChestBehavior_ItemDrop;
+        }
+
+        private void ChestBehavior_ItemDrop(On.RoR2.ChestBehavior.orig_ItemDrop orig, ChestBehavior self)
+        {
+            string goName = self.gameObject.name.ToLower();
+            if (!goName.Contains("chest"))
+                orig.Invoke(self);
+
+
+            PickupIndex pickup;
+
+            if (goName.Contains("chest1"))
+            {
+                pickup = GetRandomItem(Chest_Percantage_Modifier.Config.ChestType.Normal);
+                if(pickup != PickupIndex.none)
+                    self.SetFieldValue("dropPickup", pickup);
+            }
+            else if (goName.Contains("chest2"))
+            {
+                pickup = GetRandomItem(Chest_Percantage_Modifier.Config.ChestType.Large);
+                if (pickup != PickupIndex.none)
+                    self.SetFieldValue("dropPickup", pickup);
+            }
+            else if (goName.Contains("goldchest"))
+            {
+                pickup = GetRandomItem(Chest_Percantage_Modifier.Config.ChestType.Golden);
+                if (pickup != PickupIndex.none)
+                    self.SetFieldValue("dropPickup", pickup);
+            }
+
+            orig.Invoke(self);
+        }
+
+        private PickupIndex GetRandomItem(Config.ChestType chestType)
+        {
+            PickupIndex item = PickupIndex.none;
+
+            int rng = random.Next(0, 101);
+            float tier1Chance = 0;
+            float tier2Chance = 0;
+            float tier3Chance = 0;
+
+            switch (chestType)
+            {
+                case Chest_Percantage_Modifier.Config.ChestType.Normal:
+                    tier1Chance = config.GetValue(1, Chest_Percantage_Modifier.Config.ChestType.Normal);
+                    tier2Chance = config.GetValue(2, Chest_Percantage_Modifier.Config.ChestType.Normal);
+                    tier3Chance = config.GetValue(3, Chest_Percantage_Modifier.Config.ChestType.Normal);
+
+                    if (rng < tier1Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier1);
+                    }
+                    else if (rng < tier1Chance + tier2Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier2);
+                    }
+                    else if (rng < tier1Chance + tier2Chance + tier3Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier3);
+                    }
+                    break;
+
+                case Chest_Percantage_Modifier.Config.ChestType.Large:
+                    tier1Chance = config.GetValue(1, Chest_Percantage_Modifier.Config.ChestType.Large);
+                    tier2Chance = config.GetValue(2, Chest_Percantage_Modifier.Config.ChestType.Large);
+                    tier3Chance = config.GetValue(3, Chest_Percantage_Modifier.Config.ChestType.Large);
+
+                    if (rng < tier1Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier1);
+                    }
+                    else if (rng < tier1Chance + tier2Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier2);
+                    }
+                    else if (rng < tier1Chance + tier2Chance + tier3Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier3);
+                    }
+                    break;
+
+                case Chest_Percantage_Modifier.Config.ChestType.Golden:
+                    tier1Chance = config.GetValue(1, Chest_Percantage_Modifier.Config.ChestType.Golden);
+                    tier2Chance = config.GetValue(2, Chest_Percantage_Modifier.Config.ChestType.Golden);
+                    tier3Chance = config.GetValue(3, Chest_Percantage_Modifier.Config.ChestType.Golden);
+
+                    if (rng < tier1Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier1);
+                    }
+                    else if (rng < tier1Chance + tier2Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier2);
+                    }
+                    else if (rng < tier1Chance + tier2Chance + tier3Chance)
+                    {
+                        item = GetRandomTierItem(ItemTier.Tier3);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            return item;
+        }
+
+        private PickupIndex GetRandomTierItem(ItemTier tier)
+        {
+            List<PickupIndex> allItems;
+            PickupIndex item;
+
+            switch (tier)
+            {
+                case ItemTier.Tier1:
+                    allItems = Run.instance.availableTier1DropList;
+                    break;
+                case ItemTier.Tier2:
+                    allItems = Run.instance.availableTier2DropList;
+                    break;
+                case ItemTier.Tier3:
+                    allItems = Run.instance.availableTier3DropList;
+                    break;
+                default:
+                    return PickupIndex.none;
+                    break;
+            }
+
+            int value = random.Next(0, allItems.Count);
+
+            item = allItems[value];
+
+            return item;
+        }
+    }
+}
